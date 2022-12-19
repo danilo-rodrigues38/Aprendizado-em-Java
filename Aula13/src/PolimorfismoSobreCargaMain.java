dffefbe9d03ccd996f0d1a2b04784a65ea501b3f
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2022-12-19 14:25:16 -0300

    Add comentário

--- Aula13/src/PolimorfismoSobreCargaMain.java
+++ Aula13/src/PolimorfismoSobreCargaMain.java
@@ -4,13 +4,13 @@ public class PolimorfismoSobreCargaMain {
     public static void main (String[] args) {
         Cachorro c = new Cachorro();
 
-        c.reagir("Olá");
-        c.reagir("Vai apanhar");
-        c.reagir(11, 45);
-        c.reagir(21, 00);
-        c.reagir(true);
-        c.reagir(false);
-        c.reagir(2, 12.5f);
-        c.reagir(17, 4.5f);
+        c.reagir("Olá");            // Abanar e Latir
+        c.reagir("Vai apanhar");    // Rosnar
+        c.reagir(11, 45);           // Abanar
+        c.reagir(21, 00);           // Ignorar
+        c.reagir(true);             // Abanar
+        c.reagir(false);            // Rosnar e Latir
+        c.reagir(2, 12.5f);         // Latir
+        c.reagir(17, 4.5f);         // Rosnar
     }
 }
